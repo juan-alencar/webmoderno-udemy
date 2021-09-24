08259ddc0a590e178d96fd0c9dd8c33a85d9cb5a
juan-alencar <juan.mr.alencar2002@gmail.com>
2021-09-23 22:29:00 -0300

    Subindo arquivos

---
+++ s4_javascript_fundamentos/aritmeticos.js
@@ -0,0 +1,16 @@
+const [a, b, c , d] = [3, 5, 1, 15]
+
+// d++ -> infix
+// ++d -> prefix
+
+// unario tem apenas um único operando
+// binario tem dois ou mais operandos
+// ternario opera com 3 operandos
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(soma, subtracao, multiplicacao, divisao, modulo)
---
+++ utilidades/utilidades.js
@@ -0,0 +1,16 @@
+
+// Métodos úteis para usar com strings
+console.log("javascript".length)            //  = 10
+console.log("javascript".charAt(4))         //  = "s"
+console.log("javascript".concat(" dev"))    //  = javascript dev
+console.log("javascript".endsWith("t"))     //  = true
+console.log("javascript".startsWith("ja"))  //  = true
+console.log("javascript".includes("t"))     //  = true
+console.log("javascript".indexOf("c"))      //  = 5
+console.log("javascript".search("r"))       //  = 6
+console.log("javascript".slice(0, 4))       //  = java
+console.log("javascript".split(""))         //  = ['j', 'a', 'v', 'a', 's', 'c', 'r', 'i', 'p', 't']
+console.log("javascript".substring(4, 6))   //  = "sc"
+console.log("JAVASCRIPT".toLowerCase())     //  = "javascript"
+console.log("javascript".toUpperCase())     //  = "JAVASCRIPT"
+console.log("   javascript".trim())            //  = "javascript"
